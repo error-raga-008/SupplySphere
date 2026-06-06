4c596cd9b8af4d9e268c7a43d2a3c932d92d1582
Krish Savaliya <krishsavaliya5522@gmail.com>
2026-06-06 14:12:12 +0530

    New Design

--- frontend/src/services/rfqService.js
+++ frontend/src/services/rfqService.js
@@ -1,3 +1,13 @@
 import api from './api'
-export const listRFQs = () => api.get('/rfqs/')
-export default { listRFQs }
+
+export const listRFQs = (params = {}) => api.get('/rfqs/', { params })
+export const getRFQ = (id) => api.get(`/rfqs/${id}/`)
+export const createRFQ = (data) => api.post('/rfqs/', data)
+export const updateRFQ = (id, data) => api.put(`/rfqs/${id}/`, data)
+export const patchRFQ = (id, data) => api.patch(`/rfqs/${id}/`, data)
+export const deleteRFQ = (id) => api.delete(`/rfqs/${id}/`)
+export const publishRFQ = (id) => api.patch(`/rfqs/${id}/publish/`)
+export const closeRFQ = (id) => api.patch(`/rfqs/${id}/close/`)
+export const cancelRFQ = (id) => api.patch(`/rfqs/${id}/cancel/`)
+
+export default { listRFQs, getRFQ, createRFQ, updateRFQ, patchRFQ, deleteRFQ, publishRFQ, closeRFQ, cancelRFQ }
